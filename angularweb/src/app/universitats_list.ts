f5710693e48941ab8d4bf7c72d35ec26625d6f3c
marc.casals.camps <marc.casals.camps@est.fib.upc.edu>
2020-03-13 19:48:28 +0100

    Disseny de botons de la llista

--- angularweb/src/app/universitats_list.ts
+++ angularweb/src/app/universitats_list.ts
@@ -3,7 +3,7 @@ import { Universitat } from './universitat'
 export const UNIVERSITATS: Universitat[] = [
 {id: 1, name: 'Universitat Politècnica de Catalunya'},
 {id: 2, name: 'Universitat de Barcelona'},
-{id: 3, name: 'Universitat Pomepu Fabra'},
+{id: 3, name: 'Universitat Pompeu Fabra'},
 {id: 4, name: 'Universitat de Girona'},
 {id: 5, name: 'Universitat de Lleida'},
 {id: 6, name: 'Universitat de Vic'}
