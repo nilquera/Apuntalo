e7a464707094b5d2e190fcf894dd70aa9f521eab
mrccasals <mrccasals@gmail.com>
2020-03-26 12:02:57 +0100

    Botó de creació de nou document, només visible als usuaris.

--- frontend/src/app/documents/documents.component.ts
+++ frontend/src/app/documents/documents.component.ts
@@ -9,10 +9,17 @@ import { DocumentsService } from '../documents.service';
 export class DocumentsComponent implements OnInit {
   id: string;
   documents;
-
+  estaLogin: boolean;
   constructor(private documentsS: DocumentsService, private route: ActivatedRoute) {
     this.id = this.route.snapshot.paramMap.get("name");
     this.documentsS.getAssigDetail(this.id).subscribe(data => this.documents = data.subjectDB.posts);
+
+    if(localStorage.getItem('mytoken') !== null){
+      this.estaLogin = true;
+    }
+    else{
+      this.estaLogin = false;
+    }
   }
   ngOnInit(): void {
   }
