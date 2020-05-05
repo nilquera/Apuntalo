a5b263385c59c250d856e9ede16ad0a5645dfd2e
SergioArcos1999 <sergi.arcos@ielesvinyes.net>
2020-05-05 14:16:51 +0200

    Web 3 TESTING completo

--- blockchain/web3-3.js
+++ blockchain/web3-3.js
@@ -4,13 +4,6 @@ var  Web3 = require('web3');
 class call_blockchain {
 
        
-        /*MyContract;
-        web3;
-        id;
-        deployedNetwork;
-        contract;
-        */
-
     constructor() {
         
         this.MyContract = require('./testingpart2/build/contracts/HelloWorld.json');
@@ -20,47 +13,71 @@ class call_blockchain {
         this.id;
         this.deployedNetwork;
     }
-
+    async update() {
+        try {
+            this.contract = new this.web3.eth.Contract(this.MyContract.abi, this.deployedNetwork.address);
+        } catch(error) {
+            console.error(error);
+        }
+    }
     async init_web3() {
         try {
             this.id = await this.web3.eth.net.getId();
             this.deployedNetwork = this.MyContract.networks[this.id];
             this.contract = new this.web3.eth.Contract(this.MyContract.abi, this.deployedNetwork.address);
-            //console.log(this.contract);
         } catch(error) {
             console.error(error);
         }
     } 
 
     async get_addresses() {
-        const data = await web3.eth.getAccounts();
+        data = await this.web3.eth.getAccounts();
         console.log(data);
         return data;
     }
     
     async address_used(account) {
-        const data = await contract.methods.isTaken(account).call();
+        const data = await this.contract.methods.isTaken(account).call();
         console.log(data);
         return data;
     }
     
     async signup_user() {
-        const data = await contract.methods.signupusr().call();
+        const data = await this.contract.methods.signupusr().call();
         console.log(data);
         return data;
     }
     
     async get_Balance(account) {
         try {
-            console.log(this.id);
-            console.log(this.contract);
             var data = await this.contract.methods.getBalance(account).call();
             console.log(data);
             return data;
         } catch (error) {
             console.error(error);
         }
-        
+    }
+
+    async send_coin(sender, reciever, amount) {
+        try{
+            var data = await this.contract.methods.sendCoin(sender, reciever, amount).send({from: cuenta1});
+            console.log(data);
+            this.update();
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    
+    async activate_account(account) {
+        try {  
+            var data = await this.contract.methods.activateAccount(account).send({from: cuenta1});
+            console.log(data);
+            this.update();
+            return data;
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
 
@@ -69,9 +86,67 @@ blockchain = new call_blockchain();
 console.log("objeto invocado");
 blockchain.init_web3();
 console.log("web3 iniciado");
+console.log("Escenario: cuenta 1 (Activada) -> 100.000 monedas en su cuenta");
+console.log("           cuenta 2 (Desactivada) -> 0 monedas en su cuenta");
+console.log("Acción: Activar cuenta 2 y enviar x monedas de cuenta 1 a cuenta 2");
+
+//accounts = blockchain.get_addresses();
+var cuenta1 = "0x97aa547e791f83288520898f849c3119175050c7";
+var cuenta2 = "0xe2b0a73bc78f65f5a7c100b6b4b758baca30a7b7";
 setTimeout(function() { 
-    blockchain.get_Balance("0x98262550b91fe12d7e53ec5a7aa604a5522238db");
-}, 50);
+    console.log("Balance cuenta 1:")
+    blockchain.get_Balance(cuenta1);
+}, 3000);
+
+setTimeout(function() { 
+    console.log("cuenta 1 está activada?");
+    blockchain.address_used(cuenta1);
+}, 6000);
+
+setTimeout(function() { 
+    console.log("cuenta 2 está activada?");
+    blockchain.address_used(cuenta2);
+    
+}, 9000);
+
+setTimeout(function() { 
+    console.log("Activando cuenta...");
+    blockchain.activate_account(cuenta2);
+    
+}, 12000);
+
+setTimeout(function() { 
+    console.log("cuenta 2 está activada?");
+    blockchain.address_used(cuenta2);
+    
+}, 16000);
+
+setTimeout(function() { 
+    console.log("Balance cuenta 2:");
+    blockchain.get_Balance(cuenta2);
+}, 19000);
+
+
+setTimeout(function() { 
+    console.log("cuenta 1 envía 50.000 monedas a cuenta 2...");
+    blockchain.send_coin(cuenta1, cuenta2, 50000);
+}, 23000);
+
+setTimeout(function() { 
+    console.log("Balance cuenta 2:");
+    blockchain.get_Balance(cuenta2);
+}, 26000);
+
+setTimeout(function() { 
+    console.log("Balance cuenta 1:");
+    blockchain.get_Balance(cuenta1);
+}, 29000);
+
+setTimeout(function() { 
+    console.log("<-------------FIN DEL TESTEO------------->");
+}, 32000);
+
+
 
 
 
