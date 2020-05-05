def7e14217a37902f9eed5c5c8198f929d4bcf68
SergioArcos1999 <sergi.arcos@ielesvinyes.net>
2020-05-05 13:04:32 +0200

    Web 3 funcional

--- blockchain/web3-3.js
+++ blockchain/web3-3.js
@@ -1,27 +1,37 @@
-
+//Web3 bueno
+var  Web3 = require('web3');
 
 class call_blockchain {
 
+       
+        /*MyContract;
+        web3;
+        id;
+        deployedNetwork;
+        contract;
+        */
+
     constructor() {
-        this.Web3 = require('web3');
+        
         this.MyContract = require('./testingpart2/build/contracts/HelloWorld.json');
         this.web3 = new Web3('http://localhost:8545');
-    
+        //this.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+        this.contract;
         this.id;
         this.deployedNetwork;
-        this.contract;
     }
 
     async init_web3() {
         try {
-            id = await web3.eth.net.getId();
-            deployedNetwork = MyContract.networks[id];
-            contract = new web3.eth.Contract(MyContract.abi, deployedNetwork.address);
+            this.id = await this.web3.eth.net.getId();
+            this.deployedNetwork = this.MyContract.networks[this.id];
+            this.contract = new this.web3.eth.Contract(this.MyContract.abi, this.deployedNetwork.address);
+            //console.log(this.contract);
         } catch(error) {
             console.error(error);
         }
-        
-    }
+    } 
+
     async get_addresses() {
         const data = await web3.eth.getAccounts();
         console.log(data);
@@ -42,8 +52,10 @@ class call_blockchain {
     
     async get_Balance(account) {
         try {
-            const data = await contract.methods.getBalance(account).call();
-            console.log(ok);
+            console.log(this.id);
+            console.log(this.contract);
+            var data = await this.contract.methods.getBalance(account).call();
+            console.log(data);
             return data;
         } catch (error) {
             console.error(error);
@@ -57,8 +69,10 @@ blockchain = new call_blockchain();
 console.log("objeto invocado");
 blockchain.init_web3();
 console.log("web3 iniciado");
-blockchain.get_Balance("b930bdda26572edb36142f69111ae4b24355b715a87cf26269f6c946119310bb");
-//console.log(dato);
+setTimeout(function() { 
+    blockchain.get_Balance("0x98262550b91fe12d7e53ec5a7aa604a5522238db");
+}, 50);
+
 
 
 
