b0d8f7fc76c0f516762ad0555f7204a7d7eb1c29
mrccasals <mrccasals@gmail.com>
2020-05-08 18:08:45 +0200

    Formulari per poder afegir usuaris (falta part backend)

--- frontend/src/app/documento/documento.component.ts
+++ frontend/src/app/documento/documento.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DocumentsService } from '../documents.service';
 import { ActivatedRoute, Router } from "@angular/router";
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-documento',
@@ -11,8 +12,13 @@ export class DocumentoComponent implements OnInit {
   id: string;
   document;
   esAutor: boolean;
+  addParticipant;
 
-  constructor(private documentS: DocumentsService, private route: ActivatedRoute, public router: Router) {
+  constructor(private formBuilder: FormBuilder, private documentS: DocumentsService, private route: ActivatedRoute, public router: Router) {
+    this.addParticipant = this.formBuilder.group({
+      name: ''
+    });
+
     this.id = this.route.snapshot.paramMap.get("name");
     this.documentS.getDocDetail(this.id).subscribe(data => {
       this.document = data.postDB;
@@ -22,6 +28,17 @@ export class DocumentoComponent implements OnInit {
     });
   }
   ngOnInit(): void {
+  }
+
+  showForm(event){
+    let myelement = document.getElementsByClassName('newParticipant') as HTMLCollectionOf<HTMLElement>;;
+
+    if(myelement[0].style.display == "block"){
+      myelement[0].style.display = "none";
+    }
+    else{
+      myelement[0].style.display = "block";
+    }
   }
 
 }
