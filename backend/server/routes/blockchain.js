ad5a11b284f5a6607002e3b8d7452d75a63bd87d
Nil Quera <nlquera97@gmail.com>
2020-05-16 12:37:42 +0200

    endpoint transacció a mitges

--- backend/server/routes/blockchain.js
+++ backend/server/routes/blockchain.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const { verifyToken } = require("../middlewares/authentication");
+const Post = require("../models/post");
+let ObjectId = require("mongoose").Types.ObjectId;
 
 // Retorna el balance de l'usuari que demana (token)
 
@@ -30,7 +32,78 @@ app.get("/bc/balance", verifyToken, async (req, res) => {
   }
 });
 
-app.get("/bc/sendCoin", verifyToken, (req, res) => {
+//Compra post (postid)
+app.get("/bc/purchase", verifyToken, (req, res) => {
+  let { blockchain } = require("../server");
+  let { postid } = req.body;
+  let uid = req.user._id;
+  let uaddress = req.user.blockchain_address;
+
+  if (!ObjectId.isValid(postid)) {
+    return res.status(400).json({
+      ok: false,
+      message: "postid must be a valid id",
+    });
+  }
+
+  Post.findById(postid, "creator price", async (err, postDB) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        err,
+      });
+    }
+    if (!postDB) {
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: `Post with id ${postid} not found`,
+        },
+      });
+    }
+
+    if (uid === postDB.creator) {
+      return res.status(400).json({
+        ok: false,
+        message: "Buyer and Seller can't be the same user",
+      });
+    }
+
+    try {
+      User.findById(
+        creator,
+        "blockchain_address",
+        (err, blockchain_address) => blockchain_address
+      ).then(async (saddress) => {
+        console.log(saddress);
+        let ubalance = await blockchain.get_Balance(uaddress);
+        // Comprovar balance > price
+        if (ubalance < postDB.price) {
+          return res.status(400).json({
+            ok: false,
+            message: "Buyer doesn't have enough money",
+          });
+        }
+
+        let transaction = await send_coin(uaddress, saddress, postDB.price);
+        console.log(transaction);
+        return;
+      });
+    } catch (e) {
+      return res.status(400).json({
+        ok: false,
+        e,
+      });
+    }
+    // finally {
+    //   console.log(postDB);
+    //   return res.json({
+    //     ok: true,
+    //     post: postDB,
+    //   });
+    // }
+  });
+
   //sender (blockchain address), receiver, amount, post a comprar
 });
 
